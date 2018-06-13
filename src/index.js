0f829e5f56cac40d281e053baf86b1151206349a
Skid <Simon_Freelance@hotmail.com>
2018-06-13 12:49:33 -0400

    Notes about using an array in the render method

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
+//Class Component
 //CCR - Create Class, Call your Constructor, render method
 class App extends React.Component {
   // step 1, declare your class
   constructor() {
     // step 2, call your constructor
     super();
+    this.state = {
+      greeting: `Hello Sandbox`,
+      bands: ["Guster", "Modest Mouse", "CCR", "Skynard", "Led Zepplin"]
+    };
   }
 
   render() {
     //step 3, render must be called
-    return <h2>Hello CodeSandbox</h2>;
+    return [
+      <h2 key="item1">{this.state.greeting}</h2>,
+      <div key="item2">Guster</div>,
+      <div key="item3">Modest Mouse</div>
+    ];
   }
 }
 
